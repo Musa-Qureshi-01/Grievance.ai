e23b1a212e0d6ade2aeba2e582629362422315b3
yashrahadve05 <yashrahadve05@gmail.com>
2026-05-14 02:15:24 +0530

    Update Prisma dependencies and integrate PostgreSQL adapter in server

--- server/src/prisma/client.js
+++ server/src/prisma/client.js
@@ -1,10 +1,17 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 const globalForPrisma = globalThis;
 
 export const prisma =
   globalForPrisma.__prisma ||
   new PrismaClient({
+    adapter: new PrismaPg(
+      new Pool({
+        connectionString: process.env.DATABASE_URL,
+      })
+    ),
     log: ['error', 'warn'],
     transactionOptions: {
       maxWait: 15000,
